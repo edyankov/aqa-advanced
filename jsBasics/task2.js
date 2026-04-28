323baa21bdbeffe64dda13b5ae7a00503bf1f50d
edyankov <yankov.edik@gmail.com>
2026-04-28 15:53:50 +0300

    Add jsBasics homework: tasks 1-4

---
+++ jsBasics/task2.js
@@ -0,0 +1,12 @@
+// Task 2: String concatenation and template string
+ 
+const firstPerson = "Mikaela";
+const secondPerson = "Luka";
+ 
+// String concatenation (using +)
+const greetingConcat = "Hello, " + firstPerson + " and " + secondPerson + "!";
+console.log(greetingConcat);
+ 
+// Template string (using backticks ``)
+const greetingTemplate = `Hello, ${firstPerson} and ${secondPerson}!`;
+console.log(greetingTemplate);
---
+++ jsBasics/task3.js
@@ -0,0 +1,7 @@
+// Task 3: Numbers and booleans
+ 
+const age = 20;
+const isAdult = age >= 18; // true if the person is an adult
+ 
+console.log(age);
+console.log(isAdult);
