70681ae03fd0b941e7a9425de1506f507f09004a
edyankov <yankov.edik@gmail.com>
2026-05-26 14:32:51 +0300

    test: check pre-commit hook

--- nodeBasics/index.js
+++ nodeBasics/index.js
@@ -5,7 +5,9 @@ console.log(chalk.red.bold('This is bold red text'));
 console.log(chalk.green.underline('Green underlined text'));
 console.log(chalk.bgYellow.black(' Text with yellow background '));
 console.log(
-  chalk.magenta('You can') + ' ' +
-  chalk.cyan('combine') + ' ' +
-  chalk.hex('#FF8800')('different colors')
+    chalk.magenta('You can') +
+        ' ' +
+        chalk.cyan('combine') +
+        ' ' +
+        chalk.hex('#FF8800')('different colors')
 );
